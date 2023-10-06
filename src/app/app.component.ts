93af849676b55acd4a17e2210061f57f436663ca
dijithdamodaran <dijithdamodaran@gmail.com>
2023-10-06 16:49:42 +0530

    angular calculater

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_calculater';
+  num1:number=0
+  num2:number=0
+  additionresult:number=0
+  subtractionresult:number=0
+  multiplicationresult:number=0
+  divitionresult:number=0
+
+  onaddition(){
+    this.additionresult=this.num1+this.num2;
+  }
+ 
+  onsubtraction(){
+    this.subtractionresult=this.num1-this.num2;
+  }
+
+  onmultiplication(){
+    this.multiplicationresult=this.num1*this.num2;
+  }
+
+  ondivition(){
+    this.divitionresult=this.num1/this.num2;
+  }
+
 }
